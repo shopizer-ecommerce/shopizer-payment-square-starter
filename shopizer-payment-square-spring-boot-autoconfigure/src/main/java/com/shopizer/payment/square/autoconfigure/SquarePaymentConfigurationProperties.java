428bc7a756862d30dfdbc431a981c3de0c73113b
shopizerecomm <csamson777@yahoo.com>
2022-03-08 20:24:21 -0500

    commons-1.0

--- shopizer-payment-square-spring-boot-autoconfigure/src/main/java/com/shopizer/payment/square/autoconfigure/SquarePaymentConfigurationProperties.java
+++ shopizer-payment-square-spring-boot-autoconfigure/src/main/java/com/shopizer/payment/square/autoconfigure/SquarePaymentConfigurationProperties.java
@@ -8,6 +8,9 @@ public class SquarePaymentConfigurationProperties {
 	/**
 	 * This will set basic properties to the module if any are required
 	 */
+	
+	//Country
+	//US, Canada, Australia, Japan, the United Kingdom, Republic of Ireland, France and Spain
 
 	
 	private String uniqueCode="payment.square";
